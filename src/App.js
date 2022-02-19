9042eff03b249a21848a4c186fc0d45ba154dd5a
shmiitg <shubhamank002@gmail.com>
2022-02-19 22:03:26 +0530

    file struture setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Header from "./parts/Header/Header";
+import Footer from "./parts/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Jai Barak</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
