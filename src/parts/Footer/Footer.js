f4f1563f6967df07faef346931d2980cbf12ee70
shohan2001 <shohanmondal3@gmail.com>
2022-03-11 12:50:33 +0530

    footCommit

--- src/parts/Footer/Footer.js
+++ src/parts/Footer/Footer.js
@@ -1,7 +1,83 @@
 import React from "react";
+import footCss from "./footer.module.css";
+import instagram from "./Instagram.png";
+import facebook from "./facebook.png";
+import barakLogo from "./barakLogo.png";
+import Barak2 from "./Barak2.png";
+import iitgLogo from "./iitgLogo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return <div></div>;
+    return (
+        <div className={footCss.content}>
+            <div className={footCss.mainwrapper}>
+                <img src={barakLogo} alt="" className={footCss.barakLogo} />
+                <div className={footCss.icons}>
+                    <img src={Barak2} alt="" className={footCss.Barak2} />
+
+                    <div className={footCss.socialmedia}>
+                        <Link to="\">
+                            {" "}
+                            <img src={facebook} alt="" />
+                        </Link>
+                        <Link to="\">
+                            {" "}
+                            <img src={instagram} alt="" />
+                        </Link>
+                    </div>
+                </div>
+                <div className={footCss.wrapper}>
+                    <div className={footCss.list}>
+                        <h1>IIT GUWAHATI</h1>
+                        <h2>Hostel Affairs Board</h2>
+                        <h2>Academic Section</h2>
+                    </div>
+                    <div className={footCss.list}>
+                        <h1>IIT GUWAHATI</h1>
+                        <h2>Hostel Affairs Board</h2>
+                        <h2>Academic Section</h2>
+                    </div>
+                    <div className={footCss.list}>
+                        <h1>IIT GUWAHATI</h1>
+                        <h2>Hostel Affairs Board</h2>
+                        <h2>Academic Section</h2>
+                    </div>
+                    <div className={footCss.list}>
+                        <h1>IIT GUWAHATI</h1>
+                        <h2>Hostel Affairs Board</h2>
+                        <h2>Academic Section</h2>
+                    </div>
+                </div>
+                <img src={iitgLogo} alt="" className={footCss.iitgLogo} />
+            </div>
+            <div className={footCss.bottom}>
+                <ul>
+                    <li>
+                        <Link to="/"> Terms of Use </Link>
+                    </li>
+                    <li className={footCss.dash}>|</li>
+                    <li>
+                        <Link to="/">Cookies </Link>
+                    </li>
+                    <li className={footCss.dash}>|</li>
+                    <li>
+                        {" "}
+                        <Link to="/">Privacy </Link>
+                    </li>
+                    <li className={footCss.dash}>|</li>
+                    <li>
+                        {" "}
+                        <Link to="/">About Us</Link>
+                    </li>
+                    <li className={footCss.dash}>|</li>
+                    <li>
+                        {" "}
+                        <Link to="/">Contact Us</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
