2a7c9a0aa9442ecdcf4b02469f3df7f196ee87e9
Shubhradeep Das <72160293+shubhrad1@users.noreply.github.com>
2022-04-13 01:42:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
-                    
+                    <ROute path="/gallery" element={<Gallery />} />
                 </Routes>
                 <Footer />
             </Router>
