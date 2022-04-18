850d953913ebe1976e1cfa47f7f78c388a136e8d
shohan2001 <shohanmondal3@gmail.com>
2022-04-18 12:19:15 +0530

    dd

--- src/pages/Home/HmcSlider/hmcSlider.js
+++ src/pages/Home/HmcSlider/hmcSlider.js
@@ -1,23 +1,22 @@
 import React from "react";
-
-import slideStyle from "./slider.module.css";
 import Slider from "react-slick";
+import slideStyle from "./slider.module.css";
 
 import leftAr from "./leftA.svg";
 import rightAr from "./rightA.svg";
+import im from "./im.png";
+// const img1 =
+//     "https://images.unsplash.com/photo-1542137722061-efd1cbdf156c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
+// const img2 =
+//     "https://images.unsplash.com/photo-1542500186-6edb30855637?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+// const img3 =
+//     "https://images.unsplash.com/photo-1515405295579-ba7b45403062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
+// const img4 =
+//     "https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
+// const img5 =
+//     "https://images.unsplash.com/photo-1649452815098-687edf2fd7fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
 
-const img1 =
-    "https://images.unsplash.com/photo-1542137722061-efd1cbdf156c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
-const img2 =
-    "https://images.unsplash.com/photo-1542500186-6edb30855637?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
-const img3 =
-    "https://images.unsplash.com/photo-1515405295579-ba7b45403062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
-const img4 =
-    "https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
-const img5 =
-    "https://images.unsplash.com/photo-1649452815098-687edf2fd7fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
-
-const images = [img1, img2, img3, img4, img2, img1, img3];
+const images = [im, im, im, im, im, im, im];
 const names = [
     "Warden",
     "Caretaker",
@@ -34,7 +33,7 @@ export default function Hmc() {
     function SampleNextArrow({ onClick }) {
         return (
             <div className={slideStyle.arrowRight} onClick={onClick}>
-                <img src={rightAr} alt="" />
+                <img src={rightAr} alt="ram" />
             </div>
         );
     }
@@ -42,7 +41,7 @@ export default function Hmc() {
     function SamplePrevArrow({ onClick }) {
         return (
             <div className={slideStyle.arrowLeft} onClick={onClick}>
-                <img src={leftAr} alt="" />
+                <img src={leftAr} alt="ram" />
             </div>
         );
     }
@@ -92,7 +91,7 @@ export default function Hmc() {
             <Slider {...settings} className={slideStyle.content}>
                 {images.map((img, index) => (
                     <div className={slideStyle.slide} key={img}>
-                        <img src={img} alt="" />
+                        <img src={img} alt="IMG" />
                         {names[index]}
                     </div>
                 ))}{" "}
