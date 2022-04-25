a7108b992da70a50ccffc89ad619cae15434a896
Shiva Rapolu <vishu.rapolu@gmail.com>
2022-04-25 15:41:53 +0530

    added scroll-to-up button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,8 @@ import About from "./About us/About.js";
 import Events from "./Events/Events";
 import Hmc from "./HmcSlider/hmcSlider.js";
 import Alumni from "./Alumni/Alumni";
+import ScrollToTop from "react-scroll-up";
+import scrolluparrow from "./scrolluparrow.jpg"
 
 const Home = () => {
   return (
@@ -17,6 +19,33 @@ const Home = () => {
       <Hmc />
       <Achievements />
       <Alumni />
+    <ScrollToTop showUnder={560} duration={500} 
+    style={
+      {
+        position: 'fixed',
+      bottom: 50,
+      right: 15,
+      cursor: 'pointer',
+      transitionDuration: '0.5s',
+      transitionTimingFunction: 'linear',
+      transitionDelay: '0s',
+     }
+    }
+    >
+      <div style={
+        {
+          color:"white",
+          backgroundColor:"white",
+          boxShadow:"0px 0px 27px 10px rgb(219, 216, 216)",
+          width:"48px",
+          height:"48px",
+          zIndex:"1",
+          borderRadius:"4px",  
+        }
+      }>
+      <img style={{zIndex:"2"}} src={scrolluparrow}/>
+      </div>
+    </ScrollToTop>
     </React.Fragment>
   );
 };
