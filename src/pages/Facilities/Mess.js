ed271f6929064b98a00d4d52e3c74502b41c64b3
Shubhradeep Das <shubhramail.das@gmail.com>
2022-12-01 00:37:41 +0530

    Updated Factlities Page

--- src/pages/Facilities/Mess.js
+++ src/pages/Facilities/Mess.js
@@ -5,13 +5,12 @@ const mess = () => {
         <div className={Mess.content}>
             <h1 className={Mess.header}>Barak Mess</h1>
             <p>
-                 We have our own operating Mess, which provides all three meals in a day. 
-                 Boarders have access to a variety of food items over a week that keeps their 
-                 health and well-being in check and is a delicacy to taste. All the possible loopholes, 
-                 such as the food quality deterioration, lack of cleanliness, and some unexpected end-moment menu changes, 
-                 are mainly tackled by our elected Mess Convenor and Joint Mess Convenor. Mess feedback is taken at regular 
-                 intervals through a form accessible by QR codes pasted at the dining tables for easy access. 
-                 Students and other campus residents who aren't Barak boarders can access our Mess by paying a subscription.
+                We have our own operating Mess, which provides all three meals
+                in a day. Boarders have access to a variety of food items over a
+                week that keeps their health and well-being in check and is a
+                delicacy to taste. Mess feedback is taken at regular intervals
+                through a form accessible by QR codes pasted at the dining
+                tables for easy access.
             </p>
             <div className={Mess.buton}>
                 <div className={Mess.bookButtom}>
